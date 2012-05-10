e26e199d3e131b55a4fd637d56f1c55df59a633c
thuongdinh <thuong.dinhhoang@asnet.com.vn>
2012-05-10 22:53:17 +0700

    Test

--- app.js
+++ app.js
@@ -48,5 +48,6 @@ authRouter(app);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
+    console.log(process.env);
 });
